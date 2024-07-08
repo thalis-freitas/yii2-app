a7365353e39f0cdee26956f20585664034302eba
Thalis Freitas <ofthalis@gmail.com>
2024-07-08 19:22:13 -0300

    Valida o formato do cep

--- src/frontend/messages/en-US/error.php
+++ src/frontend/messages/en-US/error.php
@@ -5,4 +5,5 @@ return [
     'Registration number must have 11 digits' => 'Registration number must have 11 digits',
     'Failed to upload photo.' => 'Failed to upload photo.',
     'Invalid login or password.' => 'Invalid login or password.',
+    'The zip code must be in the format XXXXX-XXX or XXXXXXXX.' => 'The zip code must be in the format XXXXX-XXX or XXXXXXXX.',
 ];
--- src/frontend/messages/pt-BR/error.php
+++ src/frontend/messages/pt-BR/error.php
@@ -5,4 +5,5 @@ return [
     'Registration number must have 11 digits' => 'O CPF deve ter 11 digitos',
     'Failed to upload photo.' => 'Falha ao carregar a foto.',
     'Invalid login or password.' => 'Login ou senha inválidos.',
+    'The zip code must be in the format XXXXX-XXX or XXXXXXXX.' => 'O CEP deve estar no formato XXXXX-XXX ou XXXXXXXX.',
 ];
